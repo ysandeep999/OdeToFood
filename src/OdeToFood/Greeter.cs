f27409920e601e9020eb83decf6df46a73da0b51
sandeep yalamanchili <ysandeep999@gmail.com>
2016-10-13 15:49:11 -0400

    Clean code for last comitted data

--- src/OdeToFood/Greeter.cs
+++ src/OdeToFood/Greeter.cs
@@ -13,16 +13,14 @@ namespace OdeToFood
     public class Greeter : IGreeter
     {
         private string _greeting;
-        private string str = " hhhhhhhh";
 
         public Greeter(IConfiguration configuration)
         {
-            //Console.Write("testinggg");
+            
             _greeting = configuration["Greetings"];
         }
         public string GetGreeting()
         {
-            //Console.Write(" test _greeting varible: " + _greeting + " str is : " + str);
             return _greeting;
         }
     }
