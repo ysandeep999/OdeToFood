94ffeb435a50b1328b6714514eb66c662bb9232d
sandeep yalamanchili <ysandeep999@gmail.com>
2016-10-14 14:31:30 -0400

    Adding Attribute Routes(annotations)

--- src/OdeToFood/Controllers/AboutController.cs
+++ src/OdeToFood/Controllers/AboutController.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
+    [Route("[Controller]/[Action]")]
     public class AboutController
     {
+        //Route("phone") or
+        //[Route("[Action]")]
         public string Phone()
         {
             return "2552218";
         }
 
+        //[Route("Adress")
+        //[Route("[Action]")]
         public string Address()
         {
             return "Bezawada";
