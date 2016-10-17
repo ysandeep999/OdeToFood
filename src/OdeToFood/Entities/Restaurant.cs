17f3226a75f30e4d19a4fc3eeeaf5a66991bb2df
sandeep yalamanchili <ysandeep999@gmail.com>
2016-10-17 12:52:13 -0400

    Created a form to record a entry

--- src/OdeToFood/Entities/Restaurant.cs
+++ src/OdeToFood/Entities/Restaurant.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace OdeToFood.Entities
+﻿namespace OdeToFood.Entities
 {
+
+    public enum CuisineType
+    {
+        none,
+        Italian,
+        French,
+        American
+    }
     public class Restaurant
     {
         public string Name { get; set; }
         public int ID { get; set; }
-        
+        public CuisineType Cuisine { get; set; }
+
     }
 }
