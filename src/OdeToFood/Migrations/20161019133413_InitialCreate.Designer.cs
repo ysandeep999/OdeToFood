36c4dad56d31ccc9f7652788540b3816e61d7ad9
sandeep yalamanchili <ysandeep999@gmail.com>
2016-10-19 09:51:51 -0400

    Adding Migrations

---
+++ src/OdeToFood/Migrations/20161019133413_InitialCreate.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using OdeToFood.Entities;
+
+namespace OdeToFood.Migrations
+{
+    [DbContext(typeof(OdeToFoodDbContext))]
+    [Migration("20161019133413_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("OdeToFood.Entities.Restaurant", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Cuisine");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 40);
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Restaurants");
+                });
+        }
+    }
+}
